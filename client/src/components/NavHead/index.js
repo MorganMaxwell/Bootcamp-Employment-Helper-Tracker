67ac6543cb460e0b8f932914b21f71e59be80f16
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-13 10:20:46 -0600

    Basic layout

---
+++ client/src/components/NavHead/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from 'bootstrap';
+
+export default function NavHead(){
+    return(
+        <div>
+            <Nav>
+                Hello navbar
+            </Nav>
+        </div>
+    )
+}
