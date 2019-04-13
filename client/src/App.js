baf6e7777f84103c3c63b2c2399d958304f1259b
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-13 11:11:05 -0600

    Grid is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import {Router, Switch} from 'react-router-dom';
 import Dash from './pages/Dash';
-=======
-import MyNavBar from "./components/MyNavBar";
->>>>>>> e6749c5f0f2d63d25713b6c18558809a07ab7eae
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <div className="App">
       <Dash></Dash>
         {/* <Router>
@@ -19,9 +14,6 @@ class App extends Component {
           <Switch exact route="/dashboard"></Switch>
         </Router> */}
       </div>
-=======
-      <MyNavBar/>
->>>>>>> e6749c5f0f2d63d25713b6c18558809a07ab7eae
     );
   };
 };
