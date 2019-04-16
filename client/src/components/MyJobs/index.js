36849a77581a7301654d4086b6f0d63ef1656ec2
Daniel <danielwmorgan@gmail.com>
2019-04-15 20:12:45 -0600

    myjobs card with tabbed list added

--- client/src/components/MyJobs/index.js
+++ client/src/components/MyJobs/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {Card, Row, Col, ListGroup, Tab} from 'react-bootstrap';
 
 export default function MyJobs(){
     return(
-        <div>
-            MyJobs
-        </div>
+        <Card>
+            <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+  <Row>
+    <Col sm={4}>
+      <ListGroup>
+        <ListGroup.Item action href="#link1">
+          Link 1
+        </ListGroup.Item>
+        <ListGroup.Item action href="#link2">
+          Link 2
+        </ListGroup.Item>
+      </ListGroup>
+    </Col>
+    <Col sm={8}>
+      <Tab.Content>
+        <Tab.Pane eventKey="#link1">
+          Hello world
+        </Tab.Pane>
+        <Tab.Pane eventKey="#link2">
+          Hello World 2
+        </Tab.Pane>
+      </Tab.Content>
+    </Col>
+  </Row>
+</Tab.Container>
+        </Card>
     )
 }
