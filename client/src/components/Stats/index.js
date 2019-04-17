29e9cdae99e7252aadf17bf3a14a35af42d60b2c
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-17 17:55:11 -0600

    Wrapped a chart in a card

--- client/src/components/Stats/index.js
+++ client/src/components/Stats/index.js
@@ -6,24 +6,13 @@ export default function Stats(){
     return(
         <div>
             <h1>Stats</h1>
-            <PieChart></PieChart>
-            {/* <Card className="card-stats">
+            <Card className="card-stats">
                 <Card.Body>
                 <Row>
-                    <Col md="4" xs="5">
-                    <div className="icon-big text-center icon-warning">
-                    </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                    <div className="numbers">
-                        <Card.Title tag="p">3,237</Card.Title>
-                        <p />
-                        <p className="card-category">Awards</p>
-                    </div>
-                    </Col>
-                    </Row>
+                    <PieChart></PieChart>    
+                </Row>
                 </Card.Body>
-            </Card> */}
+            </Card>
         </div>
     )
 }
