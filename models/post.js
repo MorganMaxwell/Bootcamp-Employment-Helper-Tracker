b986695209830e4a987e186eb5b84acf808f9f76
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-17 20:56:48 -0600

    Model updated

--- models/post.js
+++ models/post.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   text: { type: String, required: true },
   category: { type: String, required: true },
+  likes: [{ type: Schema.Types.ObjectId, ref: "user" }]
 });
 
 const Post = mongoose.model("Post", postSchema);
