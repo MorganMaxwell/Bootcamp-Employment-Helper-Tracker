5c3ba1d8f83e923e7b8c0ea8917557692697a3eb
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-17 20:23:15 -0600

    Switched to Accordion

--- client/src/components/MyJobs/index.js
+++ client/src/components/MyJobs/index.js
@@ -1,39 +1,27 @@
-import React from 'react';
-import {Card, Row, Col, ListGroup, Tab} from 'react-bootstrap';
+import React from "react";
+import { Card, Row, Col, ListGroup, Tab, Accordion } from "react-bootstrap";
 
-export default function MyJobs(){
-    return(
-                <Card>
-                    <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
-                <Row>
-            <Col sm={4}>
-            <ListGroup>
-                <ListGroup.Item action href="#link1">
-                Link 1
-                </ListGroup.Item>
-                <ListGroup.Item action href="#link2">
-                Link 2
-                </ListGroup.Item>
-                <ListGroup.Item action href="#link3">
-                Link 3
-                </ListGroup.Item>
-            </ListGroup>
-            </Col>
-            <Col sm={8}>
-            <Tab.Content>
-                <Tab.Pane eventKey="#link1">
-                Description of Job 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis ipsum non quam suscipit.
-                </Tab.Pane>
-                <Tab.Pane eventKey="#link2">
-                Description of Job 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis ipsum non quam suscipit suscipit. Nullam a ante commodo, faucibus magna id, eleifend ex. Proin mollis lorem neque. Quisque malesuada turpis id eros dignissim, a mollis tortor tristique.
-                </Tab.Pane>
-                <Tab.Pane eventKey="#link3">
-                Description of Job 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis ipsum non quam suscipit suscipit. Nullam a ante commodo, faucibus magna id, eleifend ex. Proin mollis lorem neque. Quisque malesuada turpis id eros dignissim, a mollis tortor tristique.
-                </Tab.Pane>
-            </Tab.Content>
-            </Col>
-        </Row>
-        </Tab.Container>
-                </Card>
-            )
-        }
+export default function MyJobs() {
+  return (
+    <Card>
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            Click me!
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Hello! I'm the body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="1">
+            Click me!
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+    </Card>
+  );
+}
