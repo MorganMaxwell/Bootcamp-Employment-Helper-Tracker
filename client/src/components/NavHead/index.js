69830df9590a3cf5c54a66ce72b27603f9f8dab3
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-20 10:27:06 -0600

    Quick color change to header

--- client/src/components/NavHead/index.js
+++ client/src/components/NavHead/index.js
@@ -6,7 +6,7 @@ import './style.css';
 export default function NavHead() {
     return (
         <Navbar className="topNav" bg="dark" expand="lg">
-            <Navbar.Brand id="test" href="#home">$Dev => Money</Navbar.Brand>
+            <Navbar.Brand id="test" href="#home"><span id="home-link">$(Dev.Money)</span></Navbar.Brand>
             <Nav.Link href="#">Dashboard</Nav.Link>
             <Nav.Link href="#">Profile</Nav.Link>
             <Nav.Link href="#" id="sign-out" >Sign Out</Nav.Link>
