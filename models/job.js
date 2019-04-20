832445df28d0d067ede6bda9d8bd0f011721851e
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-20 11:36:55 -0600

    Added isJob bool to job model

--- models/job.js
+++ models/job.js
@@ -5,6 +5,7 @@ const jobSchema = new Schema({
   position: { type: String, required: true },
   level: { type: String, required: true },
   salary: { type: String, required: false },
+  isJob: { type: Boolean, required: false}
 });
 
 const User = mongoose.model("Post", jobSchema);
