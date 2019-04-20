66b5c75be1102869632883e3ff35b025085643aa
Morgan M <morgan.maxwell20@gmail.com>
2019-04-20 11:20:28 -0600

    jobs

---
+++ routes/api/jobs.js
@@ -0,0 +1,13 @@
+// /api/job/
+
+const router = require("express").Router();
+const jobController = require("../../controllers/jobCtrl");
+
+router.route("/")
+    .get(jobController.findAll)
+    .post(jobController.create);
+
+router.route("/:id")
+    .get(jobController.findById)
+    .put(jobController.update)
+    .delete(jobController.remove);
