1b2e5eeb31128bdf50edb7491ed534871d421c87
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-22 21:14:59 -0600

    adding submit state to createjob modal

--- client/src/components/CreateJob/index.js
+++ client/src/components/CreateJob/index.js
@@ -2,6 +2,23 @@ import React from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 
 class CreateJob extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {level: '', position: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+      }
+
     render() {
       return (
         <Modal
