d53b560d06aac776bcca6c4ae72c284761149f22
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-25 14:22:22 -0600

    Minor color change

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -59,7 +59,8 @@ class PieChart extends Component{
                         lead //lead
                     ],
                     backgroundColor:[
-                        'rgba(255, 99, 132, 0.1)',
+                        'white',
+                        'rgba(255, 99, 132, 0.6)',
                         'rgba(54, 162, 235, 0.6)',
                         'rgba(255, 206, 86, 0.6)',
                         'rgba(75, 192, 192, 0.6)',
