f47a698587298602921b8a41fc7a3a0867adb91a
jpgaarder <quazz4life@gmail.com>
2019-04-26 21:11:58 -0600

    styling to navbar added

--- client/src/components/NavHead/index.js
+++ client/src/components/NavHead/index.js
@@ -6,7 +6,12 @@ import './style.css';
 export default function NavHead() {
     return (
         <Navbar className="topNav" bg="dark" expand="lg">
-            <Navbar.Brand id="test" href="#home"><span id="home-link">$(Dev.Money)</span></Navbar.Brand>
+            <Navbar.Brand id="test" href="#home"><span id="home-link">
+            <span class="first">$</span>
+            <span class ="parenth">(</span>
+            <span class ="dev">Dev</span>
+            <span class ="money">.Money</span>
+            <span class ="parenth">)</span></span></Navbar.Brand>
             <Nav.Link href="#">Dashboard</Nav.Link>
             <Nav.Link href="#">Profile</Nav.Link>
             <Nav.Link href="#" id="sign-out" >Sign Out</Nav.Link>
