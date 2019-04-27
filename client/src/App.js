00c8ed6255c59572b8c53e5affc0baf5aafdb384
Garret Rueckert <garret.rueckert.guitar@gmail.com>
2019-04-26 20:01:01 -0600

    Fixed react-router issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Router, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dash from './pages/Dash';
+import Login from './pages/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Dash></Dash>
-        {/* <Router>
-          <Switch exact route="/"></Switch>
-          <Switch exact route="/login"></Switch>
-          <Switch exact route="/dashboard"></Switch>
-        </Router> */}
+        <Router>
+          <Switch>
+            <Route exact path="/"/>
+            <Route path="/login" exact component={Login}/>
+            <Route path="/dashboard" exact component={Dash}/>
+          </Switch>
+        </Router>
       </div>
     );
   };
---
+++ client/src/pages/Login.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Container, Col, Row, Button } from "react-bootstrap";
+
+class Login extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h2>Login Here</h2>
+            <form class="login">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email-input"
+                  placeholder="Email"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password-input"
+                  placeholder="Password"
+                />
+              </div>
+              <button type="submit" class="btn btn-dark" id="loginbtn">
+                Login
+              </button>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Login;
