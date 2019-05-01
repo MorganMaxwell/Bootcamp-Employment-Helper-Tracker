43c5e917d9be27717bd6d65338cd55e27f0d6369
Morgan M <morgan.maxwell20@gmail.com>
2019-05-01 16:44:18 -0600

    if-env

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ const routes = require("./routes");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
+// };
     app.use(express.static("client/build"));
-};
+
 
 app.use(routes);
 
